0ca7ea9a3dd38052574fc61c9df51fe41c5a9778
Ilyas <ilyassayfutdinov0087@gmail.com>
2024-04-10 14:43:58 +0300

    Fixed migrations

---
+++ WebApp1/Migrations/20240410113905_categorymissed.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp1.Migrations
+{
+    /// <inheritdoc />
+    public partial class categorymissed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Advertisements",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Advertisements",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Advertisements");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Advertisements");
+        }
+    }
+}
